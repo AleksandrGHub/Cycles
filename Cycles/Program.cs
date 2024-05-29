babcf226944e6646d416869b732ed8be30674311
AleksandrGHub <bulanov.shura@gmail.com>
2024-05-29 06:30:42 +0300

    Добавьте файлы проекта.

---
+++ Cycles/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Cycles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message;
+            int quantityRepets;
+
+            Console.Write("Напишите слово, которое необходимо повторить: ");
+            message = Console.ReadLine();
+
+            Console.Write("Укажите количество повторений: ");
+            quantityRepets = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < quantityRepets; i++)
+            {
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
